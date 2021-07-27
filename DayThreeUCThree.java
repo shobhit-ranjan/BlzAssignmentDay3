baad915893dd5b3c0a010f8b2752aafb6ae9a2b4
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-07-27 22:02:56 +0530

    Third commit Day3 UC3

--- DayThreeUCThree.java
+++ DayThreeUCThree.java
@@ -1,11 +1,14 @@
 package com.day3.uc1;
 
-public class DayThreeUCTwo {
+public class DayThreeUCThree {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+
 		 int wagePerHour=20;
 		 int fullDayHour=8;
+		 int HalfDayHour=4;
 		 double randomDouble = Math.random();
 	      randomDouble = randomDouble * 10 + 1 ;
 	      int randomInt = (int) randomDouble;
@@ -17,7 +20,13 @@ public class DayThreeUCTwo {
 	            int salaryForToday=(wagePerHour*fullDayHour);
 	            System.out.println("Today you got " + salaryForToday);
 	      }
-	     
+	      else if (randomInt%5==0||randomInt%7==0)
+	      {
+	    	  System.out.println("Employee is present for halfday ");
+	    	   int salaryForToday=(wagePerHour*HalfDayHour);
+	    	   System.out.println("Today you got " + salaryForToday);
+	      }
+	      
 	      else
 	      {
 	    	  System.out.println("Employee is Abesent");
